c5ee9f166114f6f189fd506b29fbda70bcacc58b
Radosław Kintzi <r.kintzi@pobox.net.pl>
2018-03-23 08:03:44 +0100

    Remove unneeded code

--- src/app/widgets-frame/widget-content.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { WidgetContentDirective } from './widget-content.directive';
-
-describe('WidgetContentDirective', () => {
-  it('should create an instance', () => {
-    const directive = new WidgetContentDirective();
-    expect(directive).toBeTruthy();
-  });
-});
